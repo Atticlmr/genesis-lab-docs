828623628717a7be7b25bbcf53a71b5d7c454a32
Atticli <Atticlmr@gmail.com>
2025-12-15 12:00:40 +0800

    re-init doc source, use genesis doc

--- source/conf.py
+++ source/conf.py
@@ -1,30 +1,29 @@
 import os
 import sys
-
-
-YOUR_PACKAGE_NAME = "genesis_lab"  # ←←← 改这里！
-
-# Prefer local source: add monorepo root (two levels up) if it contains your package
+# Prefer local source: add monorepo root (two levels up) if it contains `genesis/`
 _local_repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
-if os.path.isdir(os.path.join(_local_repo_root, YOUR_PACKAGE_NAME)):
+if os.path.isdir(os.path.join(_local_repo_root, "genesis")):
     sys.path.insert(0, _local_repo_root)
+import genesis as gs
 
-# Dynamically import your package to get __version__
-try:
-    your_pkg = __import__(YOUR_PACKAGE_NAME)
-    __version__ = getattr(your_pkg, "__version__", "0.1.0")
-except (ImportError, AttributeError):
-    __version__ = "0.1.0"  # fallback version
+__version__ = gs.__version__
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "genesis_lab"
-copyright = "2025, LI MINGRUI"
-author = "LI MINGRUI"
+project = "Genesis"
+copyright = "2024, Genesis Developers"
+author = "Genesis Developers"
 release = __version__
 version = __version__
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
@@ -40,51 +39,54 @@ extensions = [
     "sphinx_design",
 ]
 
-# MyST Markdown extensions
-myst_enable_extensions = ["colon_fence", "dollarmath", "amsmath"]
+# https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
+myst_enable_extensions = ["colon_fence", "dollarmath", "amsmath", "dollarmath"]
+# https://github.com/executablebooks/MyST-Parser/issues/519#issuecomment-1037239655
 myst_heading_anchors = 4
 
 templates_path = ["_templates"]
-exclude_patterns = []
+# exclude_patterns = ["user_guide/reference/_autosummary/*"]
+
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
 html_theme = "pydata_sphinx_theme"
 html_logo = "_static/bigger_text.png"
 html_favicon = "_static/option2_shadow_1.svg"
 
-# Version switcher — disabled by default (remove if you don't have version_switcher.json)
-# json_url = "_static/version_switcher.json"
-# version_match = os.environ.get("READTHEDOCS_VERSION") or f"v{__version__}"
-
+json_url = "_static/version_switcher.json"
+version_match = os.environ.get("READTHEDOCS_VERSION")
+if version_match is None:
+    version_match = "v" + __version__
 html_theme_options = {
     "show_nav_level": 2,
     "use_edit_page_button": True,
     "logo": {
-        "image_dark": "_static/bigger_text_white.png",  # optional
+        "image_dark": "_static/bigger_text_white.png",
     },
-    "navbar_center": ["navbar-nav"],  # removed version-switcher to avoid error
+    "navbar_center": ["version-switcher", "navbar-nav"],
     "show_version_warning_banner": False,
-    # If you later add version_switcher.json, uncomment the lines below:
-    # "switcher": {
-    #     "json_url": json_url,
-    #     "version_match": version_match,
-    # },
+    "switcher": {
+        "json_url": json_url,
+        "version_match": version_match,
+    },
 }
-
 
 html_context = {
     "display_github": True,
-    "github_user": "Atticlmr",      
-    "github_repo": "genesis_lab_doc",    
+    "github_user": "genesis-embodied-ai",
+    "github_repo": "genesis-doc",
     "github_version": "main",
     "conf_py_path": "/source/",
     "doc_path": "/source",
 }
-
-html_css_files = ["css/custom.css"]
+html_css_files = [
+    "css/custom.css",
+]
 html_static_path = ["_static"]
 
-# -- Autodoc settings --------------------------------------------------------
+### Autodoc configurations ###
 autodoc_typehints = "signature"
 autodoc_typehints_description_target = "all"
 autodoc_default_flags = ["members", "show-inheritance", "undoc-members"]
